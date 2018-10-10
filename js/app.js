ea755492f6233c81e882fc282101b46f8f55fe32
Jayce Azua <jayce.azua@gmail.com>
2018-10-10 02:53:17 -0700

    done?

--- js/app.js
+++ js/app.js
@@ -5,27 +5,35 @@ const bill = document.getElementById('amount');
 const percent = document.getElementById('percent');
 const tip = document.getElementById('tip');
 const people = document.getElementById('people');
-const tipEach = document.getElementById('tipEach');
-const each = document.getElementById('each');
 const total = document.getElementById('total');
 const calculate = document.getElementById('calculate');
 
+// Each Person
+const tipEach = document.getElementById('tipEach');
+const eachTotal = document.getElementById('eachTotal');
 
 // Set the content and style of an element
 tip.innerHTML = '0.00';
 total.innerHTML = '0.00';
-each.innerHTML = '0';
-tipEach.innerHTML = '0.00';
+
+eachTotal.innerHTML = '0.00';
 tipEach.innerHTML = '0.00';
 
 calculate.onclick = (e) => {
- const percentReal = parseInt(percent.value) / 100;
- const billReal = parseInt(bill.value);
- const peopleReal = parseInt(people.value);
- const tipReal = percentReal * billReal;
- const billPlusTip = tipReal + billReal
- tip.innerHTML = tipReal.toFixed(2); // parseInt(tipReal)
- total.innerHTML = billPlusTip.toFixed(2);
- const eachBill = billPlusTip / peopleReal
- each.innerHTML = eachBill.toFixed(2)
-}
+    const billReal = parseInt(bill.value);
+    const percentReal = parseInt(percent.value) / 100;
+    const peopleReal = parseInt(people.value);
+    const tipReal = percentReal * billReal;
+    const billPlusTip = tipReal + billReal
+
+    tip.innerHTML = tipReal.toFixed(2);
+    total.innerHTML = billPlusTip.toFixed(2);
+    /* DONE ABOVE */
+    const eachBill = billPlusTip / peopleReal
+    const eachTip = tipReal / peopleReal;
+    tipEach.innerHTML = eachTip.toFixed(2)
+    eachTotal.innerHTML = eachBill.toFixed(2)
+
+
+
+};
